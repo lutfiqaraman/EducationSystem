cd7defec5c88f4319c2b80112150f3cb07ed599f
lutfiqaraman <lutfiqaraman@gmail.com>
2021-11-01 18:00:19 +0200

    Add project files.

---
+++ EntityFramework/EducationSystemDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EducationSystem.Entities;
+using System.Data.Entity;
+
+namespace EducationSystem.EntityFramework
+{
+    public class EducationSystemDbContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        public EducationSystemDbContext()
+            :base("name = DefaultConnection")
+        {
+
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace EducationSystem.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EducationSystem.EntityFramework.EducationSystemDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EducationSystem.EntityFramework.EducationSystemDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
