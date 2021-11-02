28c635a99ca9e5c41af5bd77e91d7acbd27fab5c
lutfiqaraman <lutfiqaraman@gmail.com>
2021-11-02 14:54:30 +0200

    create categories

---
+++ Migrations/202111021236261_AddCategoriesTable.cs
@@ -0,0 +1,29 @@
+﻿namespace EducationSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCategoriesTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+
+            Sql("INSERT INTO dbo.Categories VALUES ('Web Development')");
+            Sql("INSERT INTO dbo.Categories VALUES ('PRogramming Language')");
+
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Categories");
+        }
+    }
+}
