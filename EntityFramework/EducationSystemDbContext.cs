4af46b5c3b34dd12aa4b6d5153a815d3eceb43d1
lutfiqaraman <lutfiqaraman@gmail.com>
2021-11-04 12:22:05 +0200

    Build Course Configuration class

--- EntityFramework/EducationSystemDbContext.cs
+++ EntityFramework/EducationSystemDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using EducationSystem.Entities;
+using EducationSystem.EntityFramework.EntityConfigurations;
 using System.Data.Entity;
 
 namespace EducationSystem.EntityFramework
@@ -17,35 +18,7 @@ namespace EducationSystem.EntityFramework
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Course>()
-                .Property(c => c.Name)
-                .IsRequired()
-                .HasMaxLength(255);
-
-            modelBuilder.Entity<Course>()
-                .Property(c => c.Description)
-                .IsRequired()
-                .HasMaxLength(2000);
-
-            modelBuilder.Entity<Course>()
-                .HasRequired(c => c.Author)
-                .WithMany(a => a.Courses)
-                .HasForeignKey(c => c.AuthorId)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Course>()
-                .HasMany(c => c.Tags)
-                .WithMany(t => t.Courses)
-                .Map(m => {
-                    m.ToTable("CourseTags");
-                    m.MapLeftKey("CourseId");
-                    m.MapRightKey("TagId");
-                });
-
-            modelBuilder.Entity<Course>()
-                .HasRequired(c => c.Cover)
-                .WithRequiredPrincipal(c => c.Course);
-
+            modelBuilder.Configurations.Add(new CourseConfiguration());
             base.OnModelCreating(modelBuilder);
         }
     }
---
+++ EntityFramework/EntityConfigurations/CourseConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using EducationSystem.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace EducationSystem.EntityFramework.EntityConfigurations
+{
+    public class CourseConfiguration : EntityTypeConfiguration<Course>
+    {
+        public CourseConfiguration()
+        {
+
+            // Start :: Fields Configuration
+
+            Property(c => c.Description)
+                .IsRequired()
+                .HasMaxLength(2000);
+
+            Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            // End :: Fields Configuration
+    
+            // Start :: Relationship Configuration
+
+            HasRequired(c => c.Author)
+                .WithMany(a => a.Courses)
+                .HasForeignKey(c => c.AuthorId)
+                .WillCascadeOnDelete(false);
+
+            HasRequired(c => c.Cover)
+                .WithRequiredPrincipal(c => c.Course);
+
+            HasMany(c => c.Tags)
+                .WithMany(t => t.Courses)
+                .Map(m => {
+                    m.ToTable("CourseTags");
+                    m.MapLeftKey("CourseId");
+                    m.MapRightKey("TagId");
+                });
+
+            // End :: Relationship Configuration
+        }
+    }
+}
