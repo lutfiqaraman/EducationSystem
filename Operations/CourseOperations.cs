993ff4e31a9e3a4640f792bc17c266c608844e95
lutfiqaraman <lutfiqaraman@gmail.com>
2021-11-06 18:56:27 +0200

    Apply clean code

---
+++ Operations/CourseOperations.cs
@@ -0,0 +1,60 @@
+﻿using EducationSystem.Entities;
+using EducationSystem.EntityFramework;
+using System;
+using System.Linq;
+
+namespace EducationSystem.Operations
+{
+    public class CourseOperations
+    {
+        EducationSystemDbContext context = new EducationSystemDbContext();
+
+        public void GetSomeCoursesByLinqSyntax()
+        {
+            IQueryable<Course> query =
+                from c in context.Courses
+                where c.Name.Contains("c#")
+                orderby c.Name
+                select c;
+
+            Console.WriteLine("The courses are using LINQ syntax: ");
+
+            foreach (var course in query)
+            {
+                Console.WriteLine("- " + course.Name);
+
+            }
+
+            int count =
+                (from c in context.Courses
+                 where c.Name.Contains("c#")
+                 select c.Name
+                 ).Count();
+
+            Console.WriteLine();
+            Console.WriteLine("You have {0} courses", count);
+            Console.WriteLine();
+        }
+
+        public void GetSomeCoursesByExtensionMethod()
+        {
+            IQueryable<Course> courses = context.Courses
+                .Where(c => c.Name.Contains("c#"))
+                .OrderBy(c => c.Name);
+
+            Console.WriteLine("The courses are using Extension methods: ");
+
+            foreach (var item in courses)
+            {
+                Console.WriteLine("- " + item.Name);
+            }
+
+            int count = context.Courses
+                .Where(c => c.Name.Contains("c#"))
+                .Count();
+
+            Console.WriteLine();
+            Console.WriteLine("You have {0} courses", count);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using EducationSystem.Entities;
 using EducationSystem.EntityFramework;
+using EducationSystem.Operations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,57 +13,10 @@ namespace EducationSystem
     {
         static void Main(string[] args)
         {
-            EducationSystemDbContext context = new EducationSystemDbContext();
+            CourseOperations courses = new CourseOperations();
 
-            // START :: LINQ Syntax
-            IQueryable<Course> query =
-                from c in context.Courses
-                where c.Name.Contains("c#")
-                orderby c.Name
-                select c;
-
-            Console.WriteLine("The courses are using LINQ syntax: ");
-
-            foreach (var course in query)
-            {
-                Console.WriteLine("- " + course.Name);
-
-            }
-
-            int count =
-                (from c in context.Courses
-                 where c.Name.Contains("c#")
-                 select c.Name
-                 ).Count();
-
-            Console.WriteLine();
-            Console.WriteLine("You have {0} courses", count);
-            Console.WriteLine();
-
-            // END :: LINQ Syntax
-
-            Console.WriteLine();
-
-            // START :: Extension Method
-
-            var courses = context.Courses
-                .Where(c => c.Name.Contains("c#"))
-                .OrderBy(c => c.Name);
-
-            Console.WriteLine("The courses are using Extension methods: ");
-
-            foreach (var item in courses)
-            {
-                Console.WriteLine("- " + item.Name);
-            }
-
-            count = context.Courses
-                .Where(c => c.Name.Contains("c#"))
-                .Count();
-
-            Console.WriteLine();
-            Console.WriteLine("You have {0} courses", count);
-            // END :: Extension Method
+            courses.GetSomeCoursesByLinqSyntax();
+            courses.GetSomeCoursesByExtensionMethod();
 
             Console.ReadKey();
         }
