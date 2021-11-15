acdce638a6934d36ffd2a18a4fee1cd33d2f1406
lutfiqaraman <lutfiqaraman@gmail.com>
2021-11-15 21:31:49 +0200

    Build Generic Repository

---
+++ Core/Repositories/Repository.cs
@@ -0,0 +1,52 @@
+﻿using EducationSystem.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace EducationSystem.Core.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly EducationSystemDbContext Context;
+
+        public Repository(EducationSystemDbContext context)
+        {
+            Context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
