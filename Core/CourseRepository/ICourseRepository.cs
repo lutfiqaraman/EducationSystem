f41cc1a582ad76b206ead21ca9624ba4377d9cca
lutfiqaraman <lutfiqaraman@gmail.com>
2021-11-17 20:00:59 +0200

    Build course repository

---
+++ Core/CourseRepository/ICourseRepository.cs
@@ -0,0 +1,16 @@
+﻿using EducationSystem.Core.Repositories;
+using EducationSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationSystem.Core.CourseRepository
+{
+    public interface ICourseRepository : IRepository<Course>
+    {
+        IEnumerable<Course> GetTopSellingCourses(int count);
+        IEnumerable<Course> GetCourseWithAuthor(int pageIndex, int pageSize);
+    }
+}
